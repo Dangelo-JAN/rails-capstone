33c316eb358c1546c4177e22627f82c96c148b11
Dangelo Arrivillaga <98621809+Dangelo-JAN@users.noreply.github.com>
2022-09-10 14:29:25 -0400

    Create model specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :groups, dependent: :destroy
   has_many :expenses, dependent: :destroy
+
+  validates :name, presence: true
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
-RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, type: :model do
+  before :each do
+    @user = User.create(
+      name: 'name',
+      email: 'test@email.com',
+      password: '123456',
+      password_confirmation: '123456'
+    )
+  end
+
+  describe 'User model' do
+    it 'should be valid' do
+      expect(@user).to be_valid
+    end
+
+    it 'should have a name' do
+      @user.name = nil
+      expect(@user).to_not be_valid
+    end
+
+    it 'should have an email' do
+      @user.email = nil
+      expect(@user).to_not be_valid
+    end
+
+    it 'should have a password' do
+      @user.password = nil
+      expect(@user).to_not be_valid
+    end
+
+    it 'should have a unique name' do
+      @user2 = User.create(
+        name: 'name',
+        email: 'test@email.com',
+        password: '123456',
+        password_confirmation: '123456'
+      )
+      expect(@user2).to_not be_valid
+    end
+  end
 end
